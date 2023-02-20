ba2bf4d7ab054840fc019b75c58f73ef3611a9c0
Gaurav Prajapati <gauravprajapati094@gmail.com>
2023-02-20 15:40:06 +0530

    crud

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+from django.urls import reverse
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -13,3 +16,7 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+# this is will find the location for specific post detail
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={"pk": self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from django.views.generic import (
+
+    ListView, DetailView, CreateView,
+    UpdateView,
+    DeleteView
+)
 
 # from django.http import HttpResponse
 
 from .models import Post
+
+from django import forms
 
 
 # posts = [
@@ -27,6 +37,70 @@ def home(request):
         'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    # giving the model form which it queury the data
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+
+    context_object_name = 'posts'
+
+# sorting the post on the bases of date post
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    '''
+    we have tow option to use template 
+    1.-make template with   #<app>/<model>_<viewtype>.html exp. post_detail.html that we are using 
+    2. use template by name as we are used in above class PostListView 
+    '''
+    # template_name = "TEMPLATE_NAME"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+# orriding the mdethod from_valid to give the auther of the form is user
+    def form_valid(self, forms):
+        forms.instance.auther = self.request.user
+        return super().form_valid(forms)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+# this is will check that other people is not able to update the other post
+# if this function and UserPassesTestMixin will not there then anyone can update anyone post
+
+    def test_func(self):
+        post = self.get_object()
+        if (self.request.user) == post.auther:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    # template_name = "TEMPLATE_NAME"
+
+    def test_func(self):
+        post = self.get_object()
+        if (self.request.user) == post.auther:
+            return True
+        else:
+            return False
 
 
 def about(request):
